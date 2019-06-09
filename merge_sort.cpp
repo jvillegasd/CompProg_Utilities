7fe31922961a131df2ae54d6b34ef960e19091b2
Johnny Villegas <linkrsfan@gmail.com>
2019-06-08 22:44:40 -0500

    Add files via upload

---
+++ merge_sort.cpp
@@ -0,0 +1,25 @@
+//Modificado de http://www.inf.fh-flensburg.de/lang/algorithmen/sortieren/merge/mergen.htm
+void merge(int a[], int l, int m, int r){
+    int i = l, j = m+1, bsize = 0, cnt = 0, b[1000100];
+    while(i<=m && j<=r){
+        if(a[i]<=a[j]) b[bsize++]=a[i++], ans+=cnt;
+        else b[bsize++]=a[j++], cnt++;
+    }
+    while(i<=m) b[bsize++]=a[i++], ans+=cnt;
+    while(j<=r) b[bsize++]=a[j++];
+    for(int k = 0; k < bsize; k++) a[l+k] = b[k];
+}
+
+void mergesort(int a[], int l, int r){
+    if(l >= r) return;
+    int m = (l+r)/2;
+    mergesort(a, l, m);
+    mergesort(a, m+1, r);
+    merge(a, l, m, r);
+}
+
+int main(){
+    int a[1000100], n;
+    mergesort(a, 0, n-1);
+    return 0;
+}
