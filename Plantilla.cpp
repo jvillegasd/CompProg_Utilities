220250f1884dea2e19e63151030e54cfa298695a
Johnny Villegas <linkrsfan@gmail.com>
2019-07-04 15:00:20 -0500

    Update Plantilla.cpp

--- Plantilla.cpp
+++ Plantilla.cpp
@@ -20,7 +20,7 @@ vector<int> g[0];
 
 lli gcd(lli a, lli b){if(b == 0) return a;a%=b;return gcd(b, a);}
 lli lcm(lli a, lli b){return (a*b)/gcd(a, b);}
-int bins(lli sum){int l=0,r=n-1,mid;while(l<=r){mid=(l+r)/2;if(arr[mid]==sum)return mid;if(arr[mid]<sum)l=mid+1;else r=mid-1;}return -1;}
+int bins(lli n,lli arr[],lli sum){int l=0,r=n-1,mid;while(l<=r){mid=(l+r)/2;if(arr[mid]==sum)return mid;if(arr[mid]<sum)l=mid+1;else r=mid-1;}return -1;}
 
 int main(){
    
