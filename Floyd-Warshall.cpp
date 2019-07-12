7f97aff0e1d8f4faeb0cbd64116b65c1c5eeeb48
Johnny Villegas <linkrsfan@gmail.com>
2019-07-11 23:52:56 -0500

    Add files via upload

---
+++ Floyd-Warshall.cpp
@@ -0,0 +1,14 @@
+int main(){
+    int n;
+    lli dp[510][510];
+    scanf("%d", &n);
+    for(int i = 1; i <= n; i++) for(int j = 1; j <= n; j++) scanf("%lld", &dp[i][j]);
+    for(int k = 1; k <= n; k++){
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= n; j++){
+                if(dp[i][k]+dp[k][j] < dp[i][j]) dp[i][j]=dp[i][k]+dp[k][j];
+            }
+        }
+    }
+    return 0;
+}
