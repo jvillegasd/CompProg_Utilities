f71d5c0edb5abd1e1e967aba3b2c697a026cbbea
Johnny Villegas <linkrsfan@gmail.com>
2019-07-18 23:20:57 -0500

    Update Floyd-Warshall.cpp

--- Floyd-Warshall.cpp
+++ Floyd-Warshall.cpp
@@ -1,6 +1,6 @@
 int main(){
     int n;
-    lli dp[510][510];
+    lli dp[510][510]; //0x3f memset
     scanf("%d", &n);
     for(int i = 1; i <= n; i++) for(int j = 1; j <= n; j++) scanf("%lld", &dp[i][j]);
     for(int k = 1; k <= n; k++){
