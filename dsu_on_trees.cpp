c35f39e6b2553433adac1b7339e2eb69268d2856
Johnny Villegas <linkrsfan@gmail.com>
2019-07-28 13:29:35 -0500

    Add files via upload

--- dsu_on_trees.cpp
+++ dsu_on_trees.cpp
@@ -4,7 +4,21 @@ typedef pair<int, int> pii;
 vector<int> g[1000010];
 multiset<int> dsu[1000010];
 int k;
-
+/*
+	Es la implementacion en O(nlog^2(n)) de DSU on trees. Sirve para resolver problemas donde
+	se pregunten ciertas propiedades que debe tener un nodo "u", todo esto para multiples queries.
+	El multiset hace de la estructura del dsu, aqui se iran guardando los representantes de cada conjunto,
+	esto cambia de acuerdo al problema. Una optimizacion lo cual hace que tenga la complejidad antes descrita
+	es el hecho de usar la mejora del peso del DSU original, para esto, se busca el hijo del subarbol "u" que
+	tenga mas peso y este se le asigna al DSU de "u". Dependiendo de la propiedad que te esten pidiendo, vas
+	a iterar en los demas hijos e iras buscando nodos que cumplan la propiedad especifica y lo iras uniendo
+	en su respectivo conjunto disjunto (literalmente como el DSU original), claro, dependiendo de que si entra en uno
+	ya creado o sino, lo crea. Cabe aclarar que la union tambien depende del problema. 
+	Ejemplo: La implementacion que esta aqui corresponde a un problema cuyo enunciado es:
+	Dado un arbol, se necesita el numero de conjuntos de hojas cuyas distancias "d" entre ellas sea "d"<="k";
+	para optimizar espacio solo guardabamos el representante de cada conjunto disjunto y esto se denotaba como 
+	el que tuviera una mayor distancia ya que solo nos pedia el numero de conjuntos disjuntos
+*/
 void DFS(int u, int p, int d){
     if(p && g[u].size()==1){
         dsu[u].insert(d);
